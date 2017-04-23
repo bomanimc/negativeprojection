80189b91d8bd6bdd9cbf82501b9305b771793d42
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2017-04-23 14:05:53 -0500

    Allows Node.js server to report if a hand is open or closed

--- AnimationController/server.js
+++ AnimationController/server.js
@@ -1,10 +1,23 @@
 var express = require('express');
-var app = new express();
+var Leap = require('leapjs');
 
+var app = new express();
 app.set('port', 3000);
 
+var controller = new Leap.Controller();
+controller.connect();
+
+var handState = 0;
+
+Leap.loop({
+    hand: function(hand){
+        var strength = hand.grabStrength;
+        handState = (strength > 0.90) ? 1 : 0;
+    }
+});
+
 app.get('/grip', function(req, res) {
-    res.send("HI THERE");
+    res.send({handState: handState});
 });
 
 app.listen(app.get('port'), function() {
